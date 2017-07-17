7ee84bbf7f23c27230a291bbb075b738090fadfc
ABackerNINI <1105908317@qq.com>
2017-07-17 18:37:50 +0800

    test new version of nJson

--- main.cpp
+++ main.cpp
@@ -4,84 +4,35 @@
 #include <list>
 #include <string>
 #include <string.h>
-#include <conio.h>
-#include "resource/Res.h"
+#include "resource/crms_resource/CRMS_RootResource.h"
 
 int main(){
 	printf("debug begin\n");
-	//printf("%s %d %s %s %s\n", __FILE__, __LINE__, __DATE__,__TIME__,__func__);
-	Res res;
+
+	CRMS_RootResource Res;
 	{
-		res.set_c('c');
-		res.set_b(false);
-		res.set_s(159);
-		res.set_i(10001);
-		res.set_pc("1234567890");
-		res.set_f(1000000.2);
-		res.set_ll(1234567890);
-		res.set_d(1234567890.123456);
-		res.get_l().push_back(1);
-		res.get_l().push_back(2);
-		res.get_l().push_back(3);
-		res.get_l().push_back(4);
-		res.get_m()["1"] = "2";
-		res.get_m()["2"] = "3";
-		res.get_m()["3"] = "4";
-		res.get_m()["4"] = "5";
-		std::map<std::string,std::string>m;
-		{
-			m["1"] = "2";
-			m["2"] = "\\3";
-			m["3"] = "4";
-		}
-		res.set_m(m);
-		res.get_lm().push_back(m);
-		res.get_lm().push_back(m);
-		res.get_lm().push_back(m);
-		res.get_lm().push_back(m);
-		res.get_ir().set_key("k");
-		res.get_ir().set_val("v");
-		res.set_pir(new InnerRes());
-		res.get_pir()->set_type(1);
-		res.get_pir()->set_key("pir key");
-		res.set_ppir(new InnerRes*());
-		*(res.get_ppir()) = new InnerRes();
-		(*(res.get_ppir()))->set_type(0);
-		(*(res.get_ppir()))->set_key("ppir key");
-		(*(res.get_ppir()))->set_val("ppir val");
-		InnerRes ir1;
-		{
-			ir1.set_type(1);
-			ir1.set_key("k1");
-			ir1.set_val("v1");
-		}
-		res.get_mir()["1"] = ir1;
-		InnerRes ir2;
-		{
-			ir2.set_type(2);
-			ir2.set_key("k2");
-			ir2.set_val("v2");
-		}
-		res.get_mir()["2"] = ir2;
+		Res.set_ri("root_res");
+		Res.set_pa("");
+		Res.set_rn("root_res");
+		Res.set_ty(1);
 	}
 
-	const char *s = serialize(res);
-	
-	printf("%s\n",s);
+	const char *s = serialize(Res,NULL);
+	{
+		printf("%s\n", s);
 
-	Res res2;
-	deserialize<Res>(&res2,s);
+		CRMS_RootResource Res2;
+		deserialize(&Res2,s);
 
-	const char *s2= serialize(res2);
+		const char *s2 = serialize(Res2,NULL);
 
-	printf("%s\n",s2);
-	
+		printf("%s\n", s2);
+
+		free((void *)s2);
+	}
 	free((void *)s);
-	free((void *)s2);
 
 	printf("debug end\n");
 
-	getch();
-	
 	return 0;
 }
