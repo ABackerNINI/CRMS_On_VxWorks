be5c26ac9707759dbb6d289e5ef84f4f1760018a
陈文杰 <1105908317@qq.com>
2017-08-10 20:57:54 +0800

    update nJson

--- protocol/resource/support/DurationSupport.h
+++ protocol/resource/support/DurationSupport.h
@@ -9,19 +9,19 @@ inline bool is_default_value(const Duration &res){
 	return true;
 }
 
-inline void set_key_value(JSON_Object *obj,const char *key,const Duration &res){
+inline void njson_set_value(JSON_Object *obj,const char *key,const Duration &res){
 	json_object_set(obj,key,"");//TODO
 }
-inline void set_key_value(JSON_Array *arr,const Duration &res){
+inline void njson_set_value(JSON_Array *arr,const Duration &res){
 	json_array_append(arr,"");//TODO
 }
 
-inline void get_value(JSON_Object *obj,const char *key,Duration *res){
+inline void njson_get_value(JSON_Object *obj,const char *key,Duration *res){
 	if(json_object_has_value(obj,key)){
 		//TODO
 	}
 }
-inline void get_value(JSON_Array *arr,size_t index,Duration *res){
+inline void njson_get_value(JSON_Array *arr,size_t index,Duration *res){
 	//TODO
 }
 
