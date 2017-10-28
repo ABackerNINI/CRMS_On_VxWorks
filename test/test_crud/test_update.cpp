ffcaa3c24ce2f17caa1c3ca6f90af76223251e83
ABacker <1105908317@qq.com>
2017-10-28 20:33:16 +0800

    test update

--- test/test_crud/test_update.cpp
+++ test/test_crud/test_update.cpp
@@ -2,6 +2,11 @@
 // Created by nini on 10/28/17.
 //
 
+#include "../Test_Tool.h"
+#include "../../protocol/resource/crms_resource/CRMS_Command.h"
+#include "../../protocol/resource/crms_resource/CRMS_Attribute.h"
+#include "../../protocol/resource/crms_resource/CRMS_ResourceObject.h"
+
 //create three resources under ROOT_RESOURCE
 //return create result
 bool test_1() {
@@ -37,30 +42,51 @@ bool test_1() {
 
 //update resources
 bool test_2() {
-    //create CRMS_ResourceObject
+    //update CRMS_ResourceObject
     crms::protocol::resource::resource::CRMS_ResourceObject ro;
     {
         ro.set_ty(crms::protocol::resource::enumeration::CRMS_ResourceType::resourceObject);
+        ro.set_onto("test_onto");
     }
-    if (!test_tool::Update("/root/1", "", &ro)) {
+    if (!test_tool::Update("/root/1", &ro)) {
         return false;
     }
 
-    //create CRMS_Attribute
+    //update CRMS_Attribute
     crms::protocol::resource::resource::CRMS_Attribute attr;
     {
         attr.set_ty(crms::protocol::resource::enumeration::CRMS_ResourceType::attribute);
+        attr.set_data("test_data");
+        attr.set_ro(true);
     }
-    if (!test_tool::Update("/root/1", "", &attr)) {
+    if (!test_tool::Update("/root/2", &attr)) {
         return false;
     }
 
-    //create CRMS_Command
+    //update CRMS_Command
     crms::protocol::resource::resource::CRMS_Command cmd;
     {
         cmd.set_ty(crms::protocol::resource::enumeration::CRMS_ResourceType::command);
     }
-    if (!test_tool::Update("/root/1", "", &cmd)) {
+    if (!test_tool::Update("/root/3", &cmd)) {
+        return false;
+    }
+
+    return true;
+}
+
+//retrieve
+bool test_3() {
+    if (!test_tool::Retrieve("/root")) {
+        return false;
+    }
+    if (!test_tool::Retrieve("/root/1")) {
+        return false;
+    }
+    if (!test_tool::Retrieve("/root/2")) {
+        return false;
+    }
+    if (!test_tool::Retrieve("/root/3")) {
         return false;
     }
 
@@ -70,7 +96,8 @@ bool test_2() {
 int main() {
     test_tool::test_func funcs[] = {
             test_1,
-            test_2
+            test_2,
+            test_3
     };
 
     test_tool::Test(funcs, sizeof(funcs) / sizeof(test_tool::test_func));
