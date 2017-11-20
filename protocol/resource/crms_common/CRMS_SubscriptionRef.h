84a3df2b31023d7fb6356389db3a182f7db8dc54
ABacker <1105908317@qq.com>
2017-11-20 18:02:58 +0800

    update CRMS_SubscriptionRef

--- protocol/resource/crms_common/CRMS_SubscriptionRef.h
+++ protocol/resource/crms_common/CRMS_SubscriptionRef.h
@@ -3,4 +3,23 @@
 #ifndef _CRMS_RESOURCE_SUBSCRIPTION_REF_H_
 #define _CRMS_RESOURCE_SUBSCRIPTION_REF_H_
 
+#include "../ResourceBase.h"
+
+namespace crms {
+
+    namespace protocol {
+
+        namespace resource {
+
+            namespace common {
+                class CRMS_SubscriptionRef {
+                public:
+
+                DEFINE_MEMBER(id, _STRING);
+                };
+            }
+        }
+    }
+}
+
 #endif //_CRMS_RESOURCE_SUBSCRIPTION_REF_H_
