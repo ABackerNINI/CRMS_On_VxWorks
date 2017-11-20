74ae4468aa82bb1aca2a82e3e56b266ea18fe155
ABackerNINI <abacker.nini@foxmail.com>
2017-11-20 10:40:49 +0800

    define _NOEXCEPT

--- protocol/common.h
+++ protocol/common.h
@@ -5,6 +5,10 @@
 
 #include "../utility/TimeUtil/DateTime.h"
 
+#ifndef _NOEXCEPT
+#define _NOEXCEPT throw()
+#endif
+
 #define LOGEVT(_Log...) printf("\033[31mLog(\033[0m%s\033[31m)\033[0m:",DateTime::now().to_string().c_str());   \
                         printf(_Log);                                                                           \
                         printf("\n")
