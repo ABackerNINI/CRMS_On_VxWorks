7ad6ecf36479e274e37f112b41b068e7f8c9b405
ABackerNINI <abacker.nini@foxmail.com>
2017-11-20 10:41:45 +0800

    add _NOEXCEPT to getter&setter

--- protocol/resource/DefMem.h
+++ protocol/resource/DefMem.h
@@ -3,23 +3,25 @@
 #ifndef _CRMS_RESOURCE_DEFINE_MEMBER_H_
 #define _CRMS_RESOURCE_DEFINE_MEMBER_H_
 
-#define _DEFINE_MEMBER(A, M, ...)                       \
-A:                                                      \
-    __VA_ARGS__ M;                                      \
-public:                                                 \
-    const __VA_ARGS__ &get_##M()const{return M;}        \
-    __VA_ARGS__ &get_##M(){return this->M;}             \
-    void set_##M(const __VA_ARGS__ &M){this->M=M;}
+#include "../common.h"
+
+#define _DEFINE_MEMBER(A, M, ...)                               \
+A:                                                              \
+    __VA_ARGS__ M;                                              \
+public:                                                         \
+    const __VA_ARGS__ &get_##M()const _NOEXCEPT {return M;}     \
+    __VA_ARGS__ &get_##M() _NOEXCEPT {return this->M;}          \
+    void set_##M(const __VA_ARGS__ &M) _NOEXCEPT {this->M=M;}
 
 #define DEFINE_MEMBER(M, ...) _DEFINE_MEMBER(private,M,__VA_ARGS__)
 
-#define _DEFINE_POINTER_MEMBER(A, M, ...)               \
-A:                                                      \
-    __VA_ARGS__ M;                                      \
-public:                                                 \
-    __VA_ARGS__ const &get_##M()const{return M;}        \
-    __VA_ARGS__ &get_##M(){return this->M;}             \
-    void set_##M(__VA_ARGS__ M){this->M=M;}
+#define _DEFINE_POINTER_MEMBER(A, M, ...)                       \
+A:                                                              \
+    __VA_ARGS__ M;                                              \
+public:                                                         \
+    __VA_ARGS__ const &get_##M()const _NOEXCEPT {return M;}     \
+    __VA_ARGS__ &get_##M() _NOEXCEPT {return this->M;}          \
+    void set_##M(__VA_ARGS__ M) _NOEXCEPT {this->M=M;}
 
 #define DEFINE_POINTER_MEMBER(M, ...) _DEFINE_POINTER_MEMBER(private,M,__VA_ARGS__)
 
