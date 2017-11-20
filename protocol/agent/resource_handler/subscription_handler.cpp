33c45dc1df2916ace6e87bd2676cf8bb705d99f1
ABacker <1105908317@qq.com>
2017-11-20 18:02:20 +0800

    add subscription handler

---
+++ protocol/agent/resource_handler/subscription_handler.cpp
@@ -0,0 +1,25 @@
+#include "subscription_handler.h"
+
+void crms::protocol::agent::resource_handler::subscription_handler::Create(
+        crms::protocol::resource::primitive::CRMS_Req *req, crms::protocol::resource::primitive::CRMS_Rsp *rsp,
+        crms::protocol::resource::resource::CRMS_Resource *res) {
+
+}
+
+void crms::protocol::agent::resource_handler::subscription_handler::Retrieve(
+        crms::protocol::resource::primitive::CRMS_Req *req, crms::protocol::resource::primitive::CRMS_Rsp *rsp,
+        crms::protocol::resource::resource::CRMS_Resource *res) {
+
+}
+
+void crms::protocol::agent::resource_handler::subscription_handler::Update(
+        crms::protocol::resource::primitive::CRMS_Req *req, crms::protocol::resource::primitive::CRMS_Rsp *rsp,
+        crms::protocol::resource::resource::CRMS_Resource *res) {
+
+}
+
+void crms::protocol::agent::resource_handler::subscription_handler::Delete(
+        crms::protocol::resource::primitive::CRMS_Req *req, crms::protocol::resource::primitive::CRMS_Rsp *rsp,
+        crms::protocol::resource::resource::CRMS_Resource *res) {
+
+}
---
+++ protocol/agent/resource_handler/subscription_handler.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#ifndef _CRMS_RESOURCE_SUBSCRIPTION_HANDLER_H_
+#define _CRMS_RESOURCE_SUBSCRIPTION_HANDLER_H_
+
+#include "../../resource/crms_primitive/CRMS_Req.h"
+#include "../../resource/crms_primitive/CRMS_Rsp.h"
+#include "../../resource/crms_resource/CRMS_Resource.h"
+
+namespace crms {
+
+    namespace protocol {
+
+        namespace agent {
+
+            namespace resource_handler {
+
+                class subscription_handler {
+                public:
+                    static void Create(resource::primitive::CRMS_Req *req, resource::primitive::CRMS_Rsp *rsp,
+                                       resource::resource::CRMS_Resource *res);
+
+                    static void Retrieve(resource::primitive::CRMS_Req *req, resource::primitive::CRMS_Rsp *rsp,
+                                         resource::resource::CRMS_Resource *res);
+
+                    static void Update(resource::primitive::CRMS_Req *req, resource::primitive::CRMS_Rsp *rsp,
+                                       resource::resource::CRMS_Resource *res);
+
+                    static void Delete(resource::primitive::CRMS_Req *req, resource::primitive::CRMS_Rsp *rsp,
+                                       resource::resource::CRMS_Resource *res);
+                };
+            }
+        }
+    }
+}
+
+#endif //_CRMS_RESOURCE_SUBSCRIPTION_HANDLER_H_
